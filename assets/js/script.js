b282156c1c5e76a6326cce59b00961a892e5faa0
Candra Fried <candracodes@gmail.com>
2021-08-20 15:49:53 -0500

    Creating initial index.html, style.css, and script.js file shells.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* 
+TODO: 
+1. Create array that stores both javascript-related questions and answers for code quiz 
+2. Fire off questions once the user hits "Start Quiz"
+3. Create a timer function that starts the moment a user gets an incorrect answer
+3. Display "Quiz over" once the clock runs out
+*/

a175e242feb6d632ac406b53f71b128fd2830076
Candra Fried <candracodes@gmail.com>
2021-08-22 00:26:35 -0500

    Added comment TODO list of how I intend to approach this application problem. Added organization to css file to plan for a show and hide toggle state for both the questions and high score divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
 /* 
-TODO: 
+GOALS: 
 1. Create array that stores both javascript-related questions and answers for code quiz 
 2. Fire off questions once the user hits "Start Quiz"
 3. Create a timer function that starts the moment a user gets an incorrect answer
 3. Display "Quiz over" once the clock runs out
 */
+
+// TODO: DEFINE NECESSARY VARIABLES AS QUERY SELECTORS:
+// <div class="question-container-show">
+// <button id="submit">Start Quiz</button>
+// <div id="questions">
+// <h3 id="response"></h3>
+// <div class="score-container-show"></div>
+// <ul id="player-info"></ul>
+
+// TODO: CREATE A VARIABLE FOR A TIMER STARTING POINT OF 2 MINUTES:
+// var timerEl = 120;
+
+// TODO: CREATE A FUNCTION TO START THE QUIZ
+// function startQuiz() {
+
+// }
+
+// TODO: CREATE A FUNCTION TO SHOW HIGH SCORES DIV AND ADD LI ELEMENTS BASED ON INPUT VALUE + # OF QUESTIONS ANSWERED CORRECTLY
+// function showHighScores () {
+
+// }
